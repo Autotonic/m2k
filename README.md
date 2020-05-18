fe15fcd458e75dc983f90f0e26e9cefcc9757a9a
aida <aida@ieatass.international>
2020-05-18 18:05:26 -0400

    refactor

---
+++ README.md
@@ -0,0 +1,6 @@
+# M2K
+Yet another midi to keystroke thing
+
+
+## Notes
+`rtmidi` installation is broken under Windows, setting [python38 in it's setup.py works](https://github.com/patrickkidd/pyrtmidi/blob/master/setup.py#L52)
--- main.py
+++ main.py
@@ -1,38 +1,47 @@
-import rtmidi
-from pynput.keyboard import Key, Controller
+import json
+from pathlib import Path
+from typing import NoReturn
+from rtmidi import RtMidiIn
+from pynput.keyboard import Controller
 
-midiin = rtmidi.RtMidiIn()
-keyboard = Controller()
 
-table = {
-        'C2': 'a',
-        'D2': 's',
-        'E2': 'd',
-        'F2': 'f',
-        'G2': 'g',
-        'F3': 'h',
-        'G3': 'j',
-        'A3': 'k',
-        'B3': 'l',
-        'C4': ';'
-        }
+class M2K:
+    def __init__(self):
+        self.config_path = Path().cwd() / "m2k.json"
+        self.table: dict = {}
+        self.midi = RtMidiIn()
+        self.keyboard = Controller()
 
-def sendkey(midi):
-    key = midi.getMidiNoteName(midi.getNoteNumber())
-    if key not in table:
-        return
-    if midi.isNoteOn():
-        keyboard.press(table[key])
-    elif midi.isNoteOff():
-        keyboard.release(table[key])
+    def set_config(self) -> NoReturn:
+        with open(str(self.config_path), "r") as j:
+            self.table = json.load(j)
 
-ports = range(midiin.getPortCount())
-if ports:
-    print('running')
-    midiin.openPort(1)
-    while True:
-        m = midiin.getMessage(0)
-        if m:
-            sendkey(m)
-else:
-    print('NO MIDI INPUT PORTS!')
+    def update_config(self, update: dict) -> NoReturn:
+        with open(str(self.config_path), "w") as new:
+            new.write(json.dumps(update))
+        self.set_config()
+
+    def send_key(self, midi_message) -> NoReturn:
+        key = midi_message.getMidiNoteName(midi_message.getNoteNumber())
+        if key not in self.table:
+            return
+        if midi_message.isNoteOn():
+            self.keyboard.press(self.table[key])
+        elif midi_message.isNoteOff():
+            self.keyboard.release(self.table[key])
+
+    def run(self) -> NoReturn:
+        ports = range(self.midi.getPortCount())
+        if ports:
+            print("Running")
+            self.midi.openPort(0)
+            while True:
+                if midi_message := self.midi.getMessage(0):
+                    self.send_key(midi_message)
+
+
+if __name__ == "__main__":
+    m2k = M2K()
+    if m2k.config_path.exists():
+        m2k.set_config()
+        m2k.run()
